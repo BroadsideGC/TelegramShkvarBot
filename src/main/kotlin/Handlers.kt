b8ce3a8f922732a5c168b00f5824a018dd940008
Kirik64rus <kzyemskov@mail.ru>
2019-05-28 01:01:19 +0300

    Улучшил код

--- src/main/kotlin/Handlers.kt
+++ src/main/kotlin/Handlers.kt
@@ -1,7 +1,10 @@
+import SettingsSpec.toxics
 import rocks.waffle.telekt.types.events.MessageEvent
 import rocks.waffle.telekt.types.events.message
 import rocks.waffle.telekt.util.replyTo
 import java.security.SecureRandom
+
+val toxiks = settings[toxics]
 
 suspend fun rollHandler(message: MessageEvent) {
     message.message.from?.id?.let {
@@ -18,13 +21,6 @@ suspend fun rollHandler(message: MessageEvent) {
     message.bot.replyTo(message, "You rolled $roll")
 }
 
-fun checkToxik(userId: Long): Boolean {
-    val toxikList = listOf<Long>(82620713)
-    if (userId in toxikList) {
-        return true
-    }
-    return false
-}
 
 suspend fun doublesHandler(message: MessageEvent) {
     message.message.from?.id?.let {
@@ -35,20 +31,15 @@ suspend fun doublesHandler(message: MessageEvent) {
     }
     val rnd = SecureRandom()
     val names = message.message.text?.replace("/doubles", "")?.split(',')?.map { it -> it.trim() } ?: emptyList()
-    //message.bot.replyTo(message, names.size.toString())
     when {
         names.isEmpty() -> message.bot.replyTo(message, "No names provided")
         names.size % 2 != 0 -> message.bot.replyTo(message, "Not even number of names")
+        names.size != names.distinct().size -> message.bot.replyTo(message, "Repeating name")
         else -> {
-            val pairNumsAll = mutableListOf<Int>()
-            pairNumsAll.addAll(1..names.size / 2)
-            val pairNums = 1..names.size / 2
-            pairNumsAll.addAll(1..names.size / 2)
-            repeat(5 + rnd.nextInt(15)) { pairNumsAll.shuffle(rnd) }
-            val pairs = pairNumsAll.zip(names)
-            val result =
-                pairNums.map { n -> pairs.filter { it.first == n }.map { it.second } }.map { it.joinToString(" - ") }
+            val result = names.shuffled(rnd).shuffled(rnd).windowed(2, 2)
             message.bot.replyTo(message, result.joinToString("\n"))
         }
     }
 }
+
+fun checkToxik(userId: Long) = userId in toxiks
--- src/main/kotlin/Settings.kt
+++ src/main/kotlin/Settings.kt
@@ -3,6 +3,7 @@ import com.uchuhimo.konf.ConfigSpec
 
 object SettingsSpec : ConfigSpec("bot") {
     val token by required<String>()
+    val toxics by optional(emptyList<Long>())
 }
 
 val settings = Config { addSpec(SettingsSpec) }.from.json.resource("settings.json")
