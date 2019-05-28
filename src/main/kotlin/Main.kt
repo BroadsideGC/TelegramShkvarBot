90cdbf5446e7d1bbb550aa088d97359970e5b561
Leonid Startsev <sandwwraith@gmail.com>
2019-05-29 02:29:59 +0300

    Add broadcast command
    Refactor event handlers a bit
    Support sending as markdown to handle graves correctly

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
 import SettingsSpec.token
+import events.Bar
+import events.Joppa
+import events.Smash
 import rocks.waffle.telekt.bot.Bot
 import rocks.waffle.telekt.contrib.filters.CommandFilter
 import rocks.waffle.telekt.dispatcher.Dispatcher
@@ -12,19 +15,23 @@ suspend fun main() {
 
     dp.messageHandler(CommandFilter("roll"), block = ::rollHandler)
     dp.messageHandler(CommandFilter("doubles"), block = ::doublesHandler)
-    dp.messageHandler(CommandFilter("bar"), block = ::barHandler)
-    dp.messageHandler(CommandFilter("unbar"), block = ::barHandler)
-    dp.messageHandler(CommandFilter("call4bar"), block = ::barHandler)
+    dp.messageHandler(
+        CommandFilter("bar", "unbar", "call4bar"),
+        block = makeHandler(Bar, "/bar", "/unbar", "/call4bar")
+    )
     dp.messageHandler(CommandFilter("tournament"), block = ::tournamentHandler)
     dp.messageHandler(CommandFilter("next"), block = ::nextBracket)
     dp.messageHandler(CommandFilter("win"), block = ::win)
     dp.messageHandler(CommandFilter("ikea"), block = ::ikea)
-    dp.messageHandler(CommandFilter("smash"), block = ::smashHandler)
-    dp.messageHandler(CommandFilter("unsmash"), block = ::smashHandler)
-    dp.messageHandler(CommandFilter("call4smash"), block = ::smashHandler)
-    dp.messageHandler(CommandFilter("jopa"), block = ::joppaHandler)
-    dp.messageHandler(CommandFilter("unjopa"), block = ::joppaHandler)
-    dp.messageHandler(CommandFilter("iditeVjopu"), block = ::joppaHandler)
+    dp.messageHandler(
+        CommandFilter("smash", "unsmash", "call4smash"),
+        block = makeHandler(Smash, "/smash", "/unsmash", "/call4smash")
+    )
+    dp.messageHandler(
+        CommandFilter("jopa", "unjopa", "iditeVjopu", ignoreCase = true),
+        block = makeHandler(Joppa, "/jopa", "/unjopa", "/iditeVjopu")
+    )
+    dp.messageHandler(CommandFilter("broadcast"), block = ::broadcast)
 
     dp.poll()
 }
