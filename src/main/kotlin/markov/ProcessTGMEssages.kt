619c505bd2628c0f70170bb3fc576b77a79b9235
Kirill Zemskov <kzyemskov@mail.ru>
2019-06-03 02:56:32 +0300

    фикс загрузки ресурса

--- src/main/kotlin/markov/ProcessTGMEssages.kt
+++ src/main/kotlin/markov/ProcessTGMEssages.kt
@@ -5,7 +5,6 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.list
-import java.io.File
 
 @Serializable
 data class TGMessage(val id: Int, val from_id: Int, val body: String?)
@@ -14,7 +13,7 @@ class R
 
 @UnstableDefault
 fun main() {
-    val file = File(R::class.java.classLoader.getResource("sh_messages_tg.json").file)
+    val file = R::class.java.classLoader.getResource("sh_messages_tg.json")
     val data = Json.nonstrict.parse(TGMessage.serializer().list, file.readText())
     runBlocking {
         data.filter { it.body != null && it.body.isNotEmpty() }.forEach {
